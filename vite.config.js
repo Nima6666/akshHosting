602c18978df22010a341099eafcf5a12b48e19cb
Nima <lamap8708@gmail.com>
2024-10-11 19:31:12 +0545

    lil fix

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  build: {
-    rollupOptions: {
-      external: ['dompurify'],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['dompurify'],
+  //   },
+  // },
 })
